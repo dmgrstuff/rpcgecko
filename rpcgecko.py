d8af6ea138b82b7f1957489dd4307ddd7848e0c4
dmgrstuff <54510230+dmgrstuff@users.noreply.github.com>
2021-02-18 16:47:06 -0500

    RPCGecko v1.1 - new icon system and other tweaks

--- rpcgecko.py
+++ rpcgecko.py
@@ -20,15 +20,17 @@ import yaml # used for parsing yaml (like config.yml and release.yml)
 
 init() # colorama init
 default_client = "798386017241399296" # the default Discord client ID
+prev_title = None # will reset the elapsed time if you switch to a different title
 
-class config: # handles loading and saving configuration and titles.yml
-    def __init__(self, cfg_file, titles):
-        self.cfg_file = cfg_file
+class config: # handles loading and saving configuration
+    def __init__(self, cfg, titles, icons):
+        self.cfg_file = cfg
         self.titles = titles
+        self.icons = icons
         
     def load(self):
         try: f = open(self.cfg_file, "r")
-        except (FileNotFoundError):
+        except (FileNotFoundError, yaml.Scanner.ScannerError):
             ui.autoconfig()
             f = open(self.cfg_file, "r")
         cf = yaml.safe_load(f)
@@ -44,12 +46,37 @@ class config: # handles loading and saving configuration and titles.yml
         return cf
 
     def load_titles(self):
+        if os.path.isfile(self.titles) == False:
+            response = requests.get("https://dmgrstuff.github.io/rpcgecko/titles.csv") # get our file from the server
+            if response.status_code == 200: # if everything is good,
+                f = open(self.titles, "w+", encoding="utf-8")
+                f.write(response.text) # write it
+            else:
+                print(exc.titles_not_found + f" ({response.status_code})")
+                input("Press Enter to close. ")
+                sys.exit(0)
+        f = open(self.titles, "r", encoding="utf-8") # there's probably a way to not have to do this twice
         titles = {}
-        read = csv.reader(open("titles.csv", "r", encoding="utf-8"))
+        read = csv.reader(f)
         for row in read:
             key = row[0]
             titles[key] = row[1:]
+        f.close()
         return titles
+
+    def load_icons(self):
+        if os.path.isfile(self.icons) == False:
+            response = requests.get("https://dmgrstuff.github.io/rpcgecko/icons.yml")
+            if response.status_code == 200:
+                f = open(self.icons, "w+")
+                f.write(response.text)
+            else:
+                print(exc.icons_not_found + f" ({response.status_code})")
+                input("Press Enter to close. ")
+                sys.exit(0)
+        f = open(self.icons, "r")
+        icons = yaml.safe_load(f)
+        return icons
 
 class interface: # handles UI functions
 
@@ -127,7 +154,7 @@ class interface: # handles UI functions
         print("9. Settings")
         print("0. Exit\n")
         choice = input("Pick an option by typing its corresponding number. ")
-        if choice == "1":
+        if choice == "1": # basic menu elif hell
             rpc.init()
             ui.presence()
         elif choice == "2":
@@ -154,7 +181,7 @@ class interface: # handles UI functions
             response = requests.get("https://dmgrstuff.github.io/rpcgecko/release.yml")
             release = yaml.safe_load(response.text)
             if release["version"] > self.version: 
-                choice = input("RPCGecko v" + str(release["version"]) + " is available. (you are using " + ui.version_str + ")\nWould you like to open the release page?\n[" + release["release_url"] + "] (y/N) ") # it's probably a good idea to tell the user what url they're about to open, right?
+                choice = input("RPCGecko v" + str(release["version"]) + " is available. (you are using " + ui.version_str + ")\n" + release["description"] + "\nWould you like to open the release page?\n[" + release["release_url"] + "] (y/N) ") # it's probably a good idea to tell the user what url they're about to open, right?
                 if choice.lower() == "y":
                     webbrowser.open(release["release_url"]) # open the page in your default browser
                     sys.exit(0)
@@ -215,20 +242,18 @@ class interface: # handles UI functions
             print(ui.header(text))
         print("── RPCGecko ──")
         print("1. IP address: " + cf["ip"])
-        print("2. Check for titles.csv updates: " + ui.truth(cf["update_csv"]))
-        print("(disable this if you modified it yourself)")
         print("\n── Presence ──")
-        print("3. Display large game icons: " + ui.truth(cf["show_icons"]))
-        print("4. Display NNID: " + (ui.truth(cf["show_nnid"])))
+        print("2. Display game icons: " + ui.truth(cf["show_icons"]))
+        print("3. Display your NNID: " + (ui.truth(cf["show_nnid"])))
         if cf["show_nnid"] == True:    
             if cf["nnid"] is None:
-                print("5. Nintendo Network ID to display: not defined")
-            else: print("5. Nintendo Network ID to display: " + cf["nnid"])
-        print("6. Discord client ID: " + cf["client_id"])
+                print("4. Nintendo Network ID to display: not defined")
+            else: print("4. Nintendo Network ID to display: " + cf["nnid"])
+        print("5. Discord client ID: " + cf["client_id"])
         print("\n── Experimental (may cause weirdness!) ──")
-        print("7. Enable custom presence text formatting: " + ui.truth(cf["custom_format"]))
+        print("6. Enable custom presence text formatting: " + ui.truth(cf["custom_format"]))
         if cf["custom_format"] == True:
-            print("8. Presence text format: " + cf["status_format"])
+            print("7. Presence text format: " + cf["status_format"])
 
         if cf != cfg.load(): print(f"\n{yellow}0. Save changes and return to menu{n}") # check for changes, not optimal but it shouldn't be too slow
         else: print("\n0. Return to menu")
@@ -239,35 +264,35 @@ class interface: # handles UI functions
             if i != "": # don't overwrite variables with empty input
                 cf["ip"] = i
         elif choice == "2":
-            cf["update_csv"] = not cf["update_csv"]
-        elif choice == "3":
             cf["show_icons"] = not cf["show_icons"]
-        elif choice == "4":
+        elif choice == "3":
             cf["show_nnid"] = not cf["show_nnid"]
-        elif choice == "5" and cf["show_nnid"] == True:
+        elif choice == "4" and cf["show_nnid"] == True:
             i = input("Enter your Nintendo Network ID: ")
             if i != "":
                 cf["nnid"] = i
-        elif choice == "6":
+        elif choice == "5":
             i = input("Enter the client ID of your Discord application (or leave blank to revert to the default) ")
             if i != "": cf["client_id"] = i
             else: cf["client_id"] = default_client
-        elif choice == "7":
+        elif choice == "6":
             cf["custom_format"] = not cf["custom_format"]
-        elif choice == "8":
+        elif choice == "7":
             i = input("Enter the format for your Discord status. Variables should be surrounded by {}. (\n(variables: title_id, title_name, title_code, title_code_short, title_icon)\n")
             if i != "": cf["status_format"] = i
         elif choice == "0": # save config and exit
             cfg.dump(cf)
             ui.menu()
-        elif choice != "": ui.settings(error=exc.invalid_input)
+        elif choice != "": ui.settings(exc.invalid_input)
         ui.settings() # loop to keep the menu open
 
 class exception_strings: # strings used for exception handling
-    invalid_input = "\nThat's not a valid input.\n"
     conn_reset = "\nThe connection was reset - attempting to reconnect..."
-    timeout = "\nThe connection timed out. Double check your console and network connection.\n"
+    icons_not_found = "\nicons.yml was not found on the server. Please try to download it manually from the GitHub repo."
+    invalid_input = "\nThat's not a valid input.\n"
     invalid_pipe = '\nMake sure the Discord client is running and try again. (InvalidPipe)\n'
+    timeout = "\nThe connection timed out. Double check your console and network connection.\n"
+    titles_not_found = "\ntitles.csv was not found on the server. Please try to download it manually from the GitHub repo."
     upd_conn_fail = "\nChecking for updates failed. Please double check your connection.\n"
     upd_keyerror = "\nChecking for updates failed because release.yml could not be parsed.\n"
     upd_latest = "\nNo updates are available.\n"
@@ -281,6 +306,8 @@ class tcp: # handles everything TCPGecko related
             print(exc.conn_reset)
             time.sleep(2) # really just gives you enough time to see the message, this is rather pointless
         except TimeoutError:
+            try: rpc.clear()
+            except AssertionError: pass # thrown if we try to send rpc events before connecting to the Discord client
             ui.cls()
             ui.menu(error=exc.timeout)
     def read_title(self, ip):
@@ -288,21 +315,21 @@ class tcp: # handles everything TCPGecko related
         read = re.findall("........" , "000" + str(format(struct.unpack(">Q", self.tcp.readmem(0x10013C10, 8))[0] , "x")))
         return (read[0] + "-" + read[1]).upper() # making title ids case insensitive, more or less
     def title_info(self, title_id):
+        try: title_icon = icons[title_id]
+        except KeyError:
+            title_icon = icons["default"] # default icon, there's probably a better way to do this
         try:
-            title_name = titles()[title_id][0]
-            title_code = titles()[title_id][1]
-            title_icon = titles()[title_id][2]
-            try: title_code_short = title_code[6:10] # truncating the product code to four letters (ex. AGME)
-            except IndexError: # which can fail on some titles that don't have a product code in titles.csv
-                title_code_short = "-" # so we handle it
-            if title_icon == "":
-                title_icon = "wiiu" # default icon, there's probably a better way to do this
+            title_name = titles[title_id][0]
+            title_code = titles[title_id][1]
+            title_code_short = title_code[6:10] # truncating the product code to four letters (ex. AGME)
+        except IndexError: # which can fail on some titles that don't have a product code
+            title_code_short = "" # so we handle it:
         except KeyError: # usually manual entry that isn't in titles.csv
             return {
                 "title_id": title_id,
-                "title_name": "", 
-                "title_code": "",
-                "title_icon": "wiiu",
+                "title_name": "-", 
+                "title_code": "-",
+                "title_icon": icons["default"],
                 "title_code_short": ""
                 }        
         return { # normal return
@@ -318,13 +345,13 @@ class rpc: # handles everything rich presence related
         self.client_id = client_id
         self.rpc = Presence(client_id)
     def init(self): # initializes the RPC connection
-        global start_time # someone will probably yell at me for using a global variable here
         try: 
             self.rpc.connect()
-            start_time = str(int(time.time())) # and yes there's probably a more pythonic way but whatever
         except InvalidPipe:
             ui.menu(exc.invalid_pipe)
     def update(self, title_id, title_name, title_code, title_code_short, title_icon, nnid=None):
+        global prev_title # Introducing global variables - the perfect solution for your terrible coding practices
+        global start_time # jokes aside it's generally not considered good to use global variables but heck
         if cf["custom_format"] == True:
             details = cf["status_format"].format(title_id=title_id, title_name=title_name, title_code=title_code, title_code_short=title_code_short, title_icon=title_icon)
         else:
@@ -333,7 +360,9 @@ class rpc: # handles everything rich presence related
             else: details = f"{title_name} ({title_code_short})" # the default status format
         if cf["show_icons"] == False: title_icon = None # simple way to prevent icons from displaying, I think
         if cf["show_nnid"] == False: small_image = None # this will also not be displayed if you don't specify a large image
-        else: small_image = "nnid"
+        else: small_image = icons["nintendo_network"]
+        if title_id != prev_title:
+            start_time = str(int(time.time()))
         self.rpc.update( # all the info that gets sent to Discord
             details=details,
             large_image=title_icon,
@@ -342,8 +371,9 @@ class rpc: # handles everything rich presence related
             small_text=nnid,
             start=start_time,
         )
+        prev_title = title_id
     def clear(self):
-        self.rpc.clear() # clears 
+        self.rpc.clear() # clears Discord status
 
 # shorthand text formatting
 b = Style.BRIGHT
@@ -351,14 +381,15 @@ n = Style.RESET_ALL
 yellow = Fore.YELLOW
 
 # Initialize classes
-ui = interface('RPCGecko', 1.0) # sets the app name and version number (for header and updater)
+ui = interface('RPCGecko', 1.1) # sets the app name and version number (for header and updater)
 exc = exception_strings()
 tcp = tcp()
 
 # Load settings and title database
-cfg = config("config.yml", "titles.yml")
+cfg = config("config.yml", "titles.csv", "icons.yml")
 cf = cfg.load()
-titles = cfg.load_titles
+titles = cfg.load_titles()
+icons = cfg.load_icons()
 
 # Init RPC class with client id from config.yml
 try: rpc = rpc(cf["client_id"]) 
